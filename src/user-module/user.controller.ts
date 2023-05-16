b775e1a7ea5396abbda68826bd0e46bd3df934b3
tanhun <tanhoang0710@gmail.com>
2023-05-16 10:01:10 +0700

    #03 NestJS Using Nodemon

--- src/user-module/user.controller.ts
+++ src/user-module/user.controller.ts
@@ -1,9 +1,28 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post } from '@nestjs/common';
+import { UserService } from './user.service';
+import { User } from './interface/user';
 
 @Controller('users')
 export class UserController {
+  constructor(private readonly userService: UserService) {}
+
   @Get()
-  getHelloUser(): string {
-    return 'Hello user';
+  getUsers(): User[] {
+    return this.userService.getUsers();
+  }
+
+  @Get(':email')
+  getUser(@Param('email') email: string) {
+    return this.userService.getUser(email);
+  }
+
+  @Post()
+  postUser(@Body() user: User): User {
+    return this.userService.addUser(user);
+  }
+
+  @Delete(':email')
+  deleteUser(@Param('email') email: string) {
+    return this.userService.deleteUser(email);
   }
 }
---
+++ src/user-module/user.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@nestjs/common';
+import { User } from './interface/user';
+
+@Injectable()
+export class UserService {
+  public users: User[];
+
+  getUsers(): User[] {
+    return this.users;
+  }
+
+  getUser(email: string): User {
+    return this.users.find((user) => user.email === email);
+  }
+
+  addUser(user: User): User {
+    this.users.push(user);
+    return user;
+  }
+
+  deleteUser(email: string): User[] {
+    const remainingUser = this.users.filter((i) => i.email !== email);
+    this.users = remainingUser;
+    return remainingUser;
+  }
+}
