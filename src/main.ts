6affd9cadb94d4831d41465e53c1b98afdb0a62b
tanhun <tanhoang0710@gmail.com>
2023-05-22 15:32:28 +0700

    20 - Swagger Module API Specs

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { SwaggerModule } from '@nestjs/swagger';
+import { createDocument } from './swagger/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
+  app.setGlobalPrefix('api/v1');
+  SwaggerModule.setup('api', app, createDocument(app));
   await app.listen(3000);
 }
 bootstrap();
---
+++ src/swagger/swagger.config.ts
@@ -0,0 +1,8 @@
+import { SwaggerConfig } from './swagger.interface';
+
+export const SWAGGER_CONFIG: SwaggerConfig = {
+  title: 'NestJS',
+  description: 'template',
+  version: '1.0',
+  tags: ['task'],
+};
